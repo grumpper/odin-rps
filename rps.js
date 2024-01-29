7b863d0a00f2beff0a84299bd2034f772b0d4b42
Bozhidar Zhelev <6785016+grumpper@users.noreply.github.com>
2024-01-29 13:09:02 +0200

    add play game func

--- rps.js
+++ rps.js
@@ -1,39 +1,95 @@
-function getComputerChoice() {
-    const rps = ["rock", "paper", "scissors"];
+function getComputerChoice(rps) {
     // Get random number between 0 and 2
     let num = Math.floor(Math.random() * 3);
     return rps[num]
 }
 
 function playRound(playerSelection, computerSelection) {
+    let msg = ""
     if (playerSelection === computerSelection) {
-        return ("It's a tie! Try again...")
+        msg = "It's a tie! Try again..."
+        alert(msg);
+        console.log(msg)
+        return [0, 0]
     }
 
     if (playerSelection === "rock" && computerSelection === "scissors") {
-        return ("You win! Rock beats scissors")
+        msg = "You win! Rock beats scissors"
+        alert(msg);
+        console.log(msg)
+        return [1, 0]
     }
     if (playerSelection === "rock" && computerSelection === "paper") {
-        return ("You lose! Paper beats rock")
+        msg = "You lose! Paper beats rock"
+        alert(msg);
+        console.log(msg)
+        return [0, 1]
     }
 
     if (playerSelection === "paper" && computerSelection === "rock") {
-        return ("You win! Paper beats rock")
+        msg = "You win! Paper beats rock"
+        alert(msg);
+        console.log(msg)
+        return [1, 0]
     }
     if (playerSelection === "paper" && computerSelection === "scissors") {
-        return ("You lose! Scissors beats paper")
+        msg = "You lose! Scissors beats paper"
+        alert(msg);
+        console.log(msg)
+        return [0, 1]
     }
 
     if (playerSelection === "scissors" && computerSelection === "paper") {
-        return ("You win! Scissors beats paper")
+        msg = "You win! Scissors beats paper"
+        alert(msg);
+        console.log(msg)
+        return [1, 0]
     }
     if (playerSelection === "scissors" && computerSelection === "rock") {
-        return ("You lose! Rock beats scissors")
+        msg = "You lose! Rock beats scissors"
+        alert(msg);
+        console.log(msg)
+        return [0, 1]
     }
 }
 
-let playerSelection = prompt("Make a choice!").toLowerCase()
-let computerSelection = getComputerChoice()
-console.log(`Your choice was: ${playerSelection}`)
-console.log(`Computer's choice was: ${computerSelection}`)
-console.log(playRound(playerSelection, computerSelection))
+function playGame() {
+    const rps = ["rock", "paper", "scissors"]
+    let playerScore = 0
+    let computerScore = 0
+    let roundsPlayed = 0
+    let playerSelection = ""
+    let computerSelection = ""
+    let gameResult = ""
+    let roundResult = []
+
+    while (playerScore + computerScore < 5) {
+        do {
+            playerSelection = prompt("Make a choice!").toLowerCase()
+        } while (rps.includes(playerSelection) === false)
+        computerSelection = getComputerChoice(rps)
+        roundMessage = `You: ${playerSelection}; Computer: ${computerSelection}`
+        alert(roundMessage)
+        console.log(roundMessage)
+        roundResult = playRound(playerSelection, computerSelection)
+        playerScore += roundResult[0]
+        computerScore += roundResult[1]
+        roundsPlayed++
+        console.log(`
+            Rounds played: ${roundsPlayed};
+            Player: ${playerScore};
+            Computer: ${computerScore};
+            \n
+        `)
+    }
+
+    if (playerScore > computerScore) {
+        gameResult = `You won ${playerScore} to ${computerScore} after ${roundsPlayed} rounds! :)`
+    } else {
+        gameResult = `You lost ${playerScore} to ${computerScore} after ${roundsPlayed} rounds! :(`
+    }
+    alert(gameResult)
+    console.log(gameResult)
+}
+
+playGame()
