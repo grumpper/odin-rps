9fe4b3309f5d9b2d2ae04d8285ba26f928e6d0c4
Bozhidar Zhelev <6785016+grumpper@users.noreply.github.com>
2024-01-29 11:53:12 +0200

    add computer choice func

--- rps.js
+++ rps.js
@@ -1,1 +1,8 @@
-console.log("Hello World!")
+function getComputerChoice() {
+    const rps = ["Rock", "Paper", "Scissors"];
+    // Get random number between 0 and 2
+    let num = Math.floor(Math.random() * 3);
+    return rps[num]
+}
+
+getComputerChoice()
