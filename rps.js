30a434539374bfe64eca4b0c73a043e83239bebe
Bozhidar Zhelev <6785016+grumpper@users.noreply.github.com>
2024-02-02 17:15:38 +0200

    add rest of logic

--- rps.js
+++ rps.js
@@ -4,44 +4,45 @@ function getComputerChoice(rps) {
     return rps[num]
 }
 
-function playRound(playerSelection, computerSelection) {
-    let msg = ""
+function playRound(playerSelection, computerSelection, divRoundResult) {
+    const result = document.createElement('p')
+    result.style.cssText = "font-size: large; font-weight: bold"
     if (playerSelection === computerSelection) {
-        msg = "It's a tie! Try again..."
-        console.log(msg)
+        result.textContent = `It's a tie (both ${playerSelection})! Try again...`
+        divRoundResult.appendChild(result)
         return [0, 0]
     }
 
     if (playerSelection === "rock" && computerSelection === "scissors") {
-        msg = "You win! Rock beats scissors"
-        console.log(msg)
+        result.textContent = "You win! Rock beats scissors"
+        divRoundResult.appendChild(result)
         return [1, 0]
     }
     if (playerSelection === "rock" && computerSelection === "paper") {
-        msg = "You lose! Paper beats rock"
-        console.log(msg)
+        result.textContent = "You lose! Paper beats rock"
+        divRoundResult.appendChild(result)
         return [0, 1]
     }
 
     if (playerSelection === "paper" && computerSelection === "rock") {
-        msg = "You win! Paper beats rock"
-        console.log(msg)
+        result.textContent = "You win! Paper beats rock"
+        divRoundResult.appendChild(result)
         return [1, 0]
     }
     if (playerSelection === "paper" && computerSelection === "scissors") {
-        msg = "You lose! Scissors beats paper"
-        console.log(msg)
+        result.textContent = "You lose! Scissors beats paper"
+        divRoundResult.appendChild(result)
         return [0, 1]
     }
 
     if (playerSelection === "scissors" && computerSelection === "paper") {
-        msg = "You win! Scissors beats paper"
-        console.log(msg)
+        result.textContent = "You win! Scissors beats paper"
+        divRoundResult.appendChild(result)
         return [1, 0]
     }
     if (playerSelection === "scissors" && computerSelection === "rock") {
-        msg = "You lose! Rock beats scissors"
-        console.log(msg)
+        result.textContent = "You lose! Rock beats scissors"
+        divRoundResult.appendChild(result)
         return [0, 1]
     }
 }
@@ -51,56 +52,52 @@ function playGame() {
     let playerScore = 0
     let computerScore = 0
     let roundsPlayed = 0
-    let playerSelection = ""
     let computerSelection = ""
-    let gameResult = ""
     let roundResult = []
 
+    const divRoundResult = document.querySelector("#round-result")
+    const divGameResult = document.querySelector("#game-result")
     const buttons = document.querySelectorAll("button")
-    buttons.forEach(
-        button => {
-            button.addEventListener(
-                'click', () => {
-                    computerSelection = getComputerChoice(rps)
-                    roundResult = playRound(button.id, computerSelection)
-                    playerScore += roundResult[0]
-                    computerScore += roundResult[1]
-                    roundsPlayed++
-                    console.log(`
+    for (button of buttons) {
+        divGameResult.replaceChildren()
+        button.addEventListener(
+            'click', () => {
+                divRoundResult.replaceChildren()
+                divGameResult.replaceChildren()
+                computerSelection = getComputerChoice(rps)
+                roundResult = playRound(button.id, computerSelection, divRoundResult)
+                playerScore += roundResult[0]
+                computerScore += roundResult[1]
+                roundsPlayed++
+                const textRoundSummary = document.createElement('p')
+                textRoundSummary.textContent = `
                         Rounds played: ${roundsPlayed};
                         Player: ${playerScore};
-                        Computer: ${computerScore};
-                        \n
-                    `)
+                        Computer: ${computerScore};`
+                divRoundResult.appendChild(textRoundSummary)
+                if (playerScore === 5) {
+                    const textGameResult = document.createElement('h2')
+                    textGameResult.textContent = `You won ${playerScore} to ${computerScore} \
+                    after ${roundsPlayed} rounds! :)`
+                    textGameResult.style.cssText = "font-weight: bold; color: green"
+                    divGameResult.replaceChildren(textGameResult)
+                    playerScore = 0
+                    computerScore = 0
+                    roundsPlayed = 0
                 }
-            )
-        })
-    // while (playerScore + computerScore < 5) {
-    //     do {
-    //         playerSelection = prompt("Make a choice!").toLowerCase()
-    //     } while (rps.includes(playerSelection) === false)
-    //     computerSelection = getComputerChoice(rps)
-    //     roundMessage = `You: ${playerSelection}; Computer: ${computerSelection}`
-    //     console.log(roundMessage)
-    //     roundResult = playRound(playerSelection, computerSelection)
-    //     playerScore += roundResult[0]
-    //     computerScore += roundResult[1]
-    //     roundsPlayed++
-    //     console.log(`
-    //         Rounds played: ${roundsPlayed};
-    //         Player: ${playerScore};
-    //         Computer: ${computerScore};
-    //         \n
-    //     `)
-    // }
-
-    if (playerScore > computerScore) {
-        gameResult = `You won ${playerScore} to ${computerScore} after ${roundsPlayed} rounds! :)`
-    } else {
-        gameResult = `You lost ${playerScore} to ${computerScore} after ${roundsPlayed} rounds! :(`
+                if (computerScore === 5) {
+                    const textGameResult = document.createElement('h2',)
+                    textGameResult.textContent = `You lost ${playerScore} to ${computerScore} \
+                    after ${roundsPlayed} rounds! :(`
+                    textGameResult.style.cssText = "font-weight: bold; color: red"
+                    divGameResult.replaceChildren(textGameResult)
+                    playerScore = 0
+                    computerScore = 0
+                    roundsPlayed = 0
+                }
+            }
+        )
     }
-    alert(gameResult)
-    console.log(gameResult)
 }
 
 playGame()
