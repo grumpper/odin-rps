bd557b420829fb99991ba642b17f08ccd6f67a90
Bozhidar Zhelev <6785016+grumpper@users.noreply.github.com>
2024-02-02 16:06:46 +0200

    add buttons, listeners & event logic

--- rps.js
+++ rps.js
@@ -8,46 +8,39 @@ function playRound(playerSelection, computerSelection) {
     let msg = ""
     if (playerSelection === computerSelection) {
         msg = "It's a tie! Try again..."
-        alert(msg);
         console.log(msg)
         return [0, 0]
     }
 
     if (playerSelection === "rock" && computerSelection === "scissors") {
         msg = "You win! Rock beats scissors"
-        alert(msg);
         console.log(msg)
         return [1, 0]
     }
     if (playerSelection === "rock" && computerSelection === "paper") {
         msg = "You lose! Paper beats rock"
-        alert(msg);
         console.log(msg)
         return [0, 1]
     }
 
     if (playerSelection === "paper" && computerSelection === "rock") {
         msg = "You win! Paper beats rock"
-        alert(msg);
         console.log(msg)
         return [1, 0]
     }
     if (playerSelection === "paper" && computerSelection === "scissors") {
         msg = "You lose! Scissors beats paper"
-        alert(msg);
         console.log(msg)
         return [0, 1]
     }
 
     if (playerSelection === "scissors" && computerSelection === "paper") {
         msg = "You win! Scissors beats paper"
-        alert(msg);
         console.log(msg)
         return [1, 0]
     }
     if (playerSelection === "scissors" && computerSelection === "rock") {
         msg = "You lose! Rock beats scissors"
-        alert(msg);
         console.log(msg)
         return [0, 1]
     }
@@ -63,25 +56,43 @@ function playGame() {
     let gameResult = ""
     let roundResult = []
 
-    while (playerScore + computerScore < 5) {
-        do {
-            playerSelection = prompt("Make a choice!").toLowerCase()
-        } while (rps.includes(playerSelection) === false)
-        computerSelection = getComputerChoice(rps)
-        roundMessage = `You: ${playerSelection}; Computer: ${computerSelection}`
-        alert(roundMessage)
-        console.log(roundMessage)
-        roundResult = playRound(playerSelection, computerSelection)
-        playerScore += roundResult[0]
-        computerScore += roundResult[1]
-        roundsPlayed++
-        console.log(`
-            Rounds played: ${roundsPlayed};
-            Player: ${playerScore};
-            Computer: ${computerScore};
-            \n
-        `)
-    }
+    const buttons = document.querySelectorAll("button")
+    buttons.forEach(
+        button => {
+            button.addEventListener(
+                'click', () => {
+                    computerSelection = getComputerChoice(rps)
+                    roundResult = playRound(button.id, computerSelection)
+                    playerScore += roundResult[0]
+                    computerScore += roundResult[1]
+                    roundsPlayed++
+                    console.log(`
+                        Rounds played: ${roundsPlayed};
+                        Player: ${playerScore};
+                        Computer: ${computerScore};
+                        \n
+                    `)
+                }
+            )
+        })
+    // while (playerScore + computerScore < 5) {
+    //     do {
+    //         playerSelection = prompt("Make a choice!").toLowerCase()
+    //     } while (rps.includes(playerSelection) === false)
+    //     computerSelection = getComputerChoice(rps)
+    //     roundMessage = `You: ${playerSelection}; Computer: ${computerSelection}`
+    //     console.log(roundMessage)
+    //     roundResult = playRound(playerSelection, computerSelection)
+    //     playerScore += roundResult[0]
+    //     computerScore += roundResult[1]
+    //     roundsPlayed++
+    //     console.log(`
+    //         Rounds played: ${roundsPlayed};
+    //         Player: ${playerScore};
+    //         Computer: ${computerScore};
+    //         \n
+    //     `)
+    // }
 
     if (playerScore > computerScore) {
         gameResult = `You won ${playerScore} to ${computerScore} after ${roundsPlayed} rounds! :)`
